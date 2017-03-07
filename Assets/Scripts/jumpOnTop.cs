561249876f432d355a819679afa4328cd5580ebc
nwong28 <n.wong48@gmail.com>
2017-03-07 12:33:27 -0800

    Jump on top ability
    When a player lands on top of another player, will print a message on
    who got hit.
    
    Fixed NullPointerException when a player left the platform

---
+++ Assets/Scripts/jumpOnTop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpOnTop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D c) {
+		if (c.gameObject.CompareTag("Player") && c.contacts[0].normal.Equals(Vector2.up)) {
+			print("Hit on Player 1");
+		}
+		else if (c.gameObject.CompareTag("Player2") && c.contacts[0].normal.Equals(Vector2.up)) {
+			print("Hit on Player 2");
+		}
+	}
+}
